60de0a3c90838d927f3519bdb8b64f7db4e68e08
Ashraf Abdulkhaliq <63349054+kershrita@users.noreply.github.com>
2023-11-30 23:20:07 +0000

    update personal information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Ashraf Abdulkhaliq",
+  title: "AI Student @ KSU | Student Ambassador @ Microsoft | Data Science Head @ IEEE KSB",
+  email: "ashrafabdulkhaliq80@gmail.com",
+  gitHub: "kershrita",
+  instagram: "",
+  linkedIn: "ashraf-abdulkhaliq",
+  medium: "ashrafabdulkhaliq",
+  twitter: "",
+  youTube: "Kershrita",
 };
 
 const primaryColor = "#4E567E";
