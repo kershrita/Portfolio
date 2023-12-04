745ce6f79e726fb6114747baf42dfd92899f062f
Ashraf Abdulkhaliq <63349054+kershrita@users.noreply.github.com>
2023-12-04 19:49:14 +0000

    edit project and change the image

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,58 +1,32 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
-
 import React from "react";
-
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/design-desk.jpeg";
+import image from "../images/high-angle-measuring-tools-desk-still-life.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Parasite Image Classifier",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "(CNN) deep learning model for classifying medical images of parasites into various species. The model is trained to recognize and differentiate between different parasite species commonly found in medical and biological imaging data.",
+    url: "https://github.com/kershrita/Parasite-Image-Classifier",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Predicting Market Costs",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Building regression model to accurately predict the costs of products within a diverse market landscape.",
+    url: "https://github.com/kershrita/Predicting-Market-Costs",
   },
   {
-    title: "My Resume Site",
+    title: "Human Activity Recognition",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A system capable of recognizing human activities based on sensor data.",
+    url: "https://github.com/kershrita/Human-Activity-Recognition",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Biometric Attendance System",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Biometric attendance system that register and track attendance using fingerprints, allowing administrators to manage attendance records easily.",
+    url: "https://github.com/kershrita/Biometric-Attendance-System",
   },
 ];
 
@@ -77,7 +51,7 @@ const Portfolio = () => {
               <p className="small">{project.description}</p>
             </div>
           ))}
-        </div>
+      </div>
       </div>
     </section>
   );
