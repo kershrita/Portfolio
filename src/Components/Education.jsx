8c50890c0da8f359a2121ebce5f8057ad8b60e1b
Ashraf Abdulkhaliq <63349054+kershrita@users.noreply.github.com>
2023-12-04 19:29:08 +0000

    update education section with my university

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 const Education = () => {
-    return(
-        <section className="light" id="education">
-            <h2>Education</h2>
-        </section>
-    )
-}
+  return (
+    <section className="padding" id="education">
+      <h2 style={{ color: "#333", textAlign: "center" }}>Education</h2>
+      <div className="content">
+        <div className="education">
+          <div className="education__item" style={{ marginBottom: "1rem", padding: "2rem" }}>
+            <h3 style={{ color: "#555" }}>Kafrelsheikh University, Kafr El Sheikh</h3>
+            <p>
+              <strong style={{ fontSize: "1.2rem" }}>Artificial Intelligence</strong>
+            </p>
+            <p style={{ fontStyle: "italic" }}>2021 - present</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Education;
